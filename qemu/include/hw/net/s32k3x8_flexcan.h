ebba8dd035a0fb7d1e064182fcd43ad9f2df7bce
Octavio <octavio.chiman@gm.com>
2026-04-22 00:31:19 -0400

    Add FlexCAN RX entry structure and update FIFO capacities

--- qemu/include/hw/net/s32k3x8_flexcan.h
+++ qemu/include/hw/net/s32k3x8_flexcan.h
@@ -9,6 +9,13 @@
 OBJECT_DECLARE_SIMPLE_TYPE(S32K3X8FlexCANState, S32K3X8_FLEXCAN)
 
 #define S32K3X8_FLEXCAN_MMIO_SIZE 0x4000U
+#define S32K3X8_FLEXCAN_LEGACY_FIFO_CAPACITY 6U
+#define S32K3X8_FLEXCAN_ENHANCED_FIFO_CAPACITY 63U
+
+typedef struct S32K3X8FlexCANRxEntry {
+    qemu_can_frame frame;
+    uint16_t idhit;
+} S32K3X8FlexCANRxEntry;
 
 struct S32K3X8FlexCANState {
     SysBusDevice parent_obj;
@@ -22,6 +29,16 @@ struct S32K3X8FlexCANState {
     uint32_t instance_id;
     uint16_t timestamp;
     uint32_t regs[S32K3X8_FLEXCAN_MMIO_SIZE / sizeof(uint32_t)];
+
+    S32K3X8FlexCANRxEntry legacy_fifo[S32K3X8_FLEXCAN_LEGACY_FIFO_CAPACITY];
+    uint8_t legacy_fifo_head;
+    uint8_t legacy_fifo_tail;
+    uint8_t legacy_fifo_count;
+
+    S32K3X8FlexCANRxEntry enhanced_fifo[S32K3X8_FLEXCAN_ENHANCED_FIFO_CAPACITY];
+    uint8_t enhanced_fifo_head;
+    uint8_t enhanced_fifo_tail;
+    uint8_t enhanced_fifo_count;
 };
 
 #endif /* HW_NET_S32K3X8_FLEXCAN_H */
